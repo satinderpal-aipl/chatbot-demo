fd21f356c7008fa164b7a09efc8f08bd864b6c97
intersoft-admin <satinderpal.aipl@gmail.com>
2019-11-08 15:38:19 +0530

    make PWA

--- src/App.js
+++ src/App.js
@@ -16,7 +16,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          test working on github all now only push gets work
+          test working on github all 
         </a>
       </header>
     </div>
